1abea661a9f4bd786dc6f5e868408c4600e8b202
Thomas Leskin <tleskin@gmail.com>
2015-09-18 13:37:29 -0600

    Add Like button

--- app/assets/javascripts/article_likes.js
+++ app/assets/javascripts/article_likes.js
@@ -4,9 +4,25 @@ var Hello = React.createClass({
   }
 });
 
+var LikeArticle = React.createClass({
+  render: function() {
+    return React.createElement("div", null, "Like Me!");
+  }
+});
+
+//
+// $(document).ready(function() {
+//   React.render(
+//     React.createElement(Hello),
+//     document.getElementById('articles')
+//   );
+// });
+
 $(document).ready(function() {
-  React.render(
-    React.createElement(Hello),
-    document.getElementById('articles')
-  );
+  $(".like-article").each(function(index, element) {
+    React.render(
+      React.createElement(LikeArticle),
+      element
+    );
+  });
 });
